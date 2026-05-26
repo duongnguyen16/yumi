310003bb02c6195d6725f84f21287138db9ce743
duongvlog16 <duongvlog16@gmail.com>
2026-05-26 11:09:42 +0700

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ['@wdp301/shared'],
 };
 
 export default nextConfig;
