03b6fbc408ec2542a81b430cdc99a39bb79377c3
Tran Duc Minh <minhsoc5@gmail.com>
2026-06-29 00:07:40 +0700

    feat: add location view schema, update location controller, and enhance location detail screens

---
+++ apps/mobile/src/app/location/[id].tsx
@@ -0,0 +1,69 @@
+import LocationDetailScreen from "@/components/location/LocationDetailScreen";
+import Badge from "@/components/ui/Badge";
+import { getLocationById } from "@/service/locationService";
+import { Stack, useLocalSearchParams } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { View } from "react-native";
+import { ActivityIndicator, Button, Dialog, Text } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function LocationDetail() {
+  const { id } = useLocalSearchParams();
+  const [locationData, setLocationData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [dialogVisible, setDialogVisible] = useState(false);
+  console.log("locationDetail", id);
+  useEffect(() => {
+    const fetchLocationData = async () => {
+      try {
+        const response = await getLocationById(id as string);
+        if (response.success) {
+          setLocationData(response.location);
+        } else {
+          console.log("Error fetching location data:", response.message);
+          setDialogVisible(true);
+        }
+      } catch (error) {
+        console.log("Error fetching location data:", error);
+        setDialogVisible(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchLocationData();
+  }, [id]);
+  if (loading) {
+    return (
+      <SafeAreaView
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
+        <ActivityIndicator size="large" />
+      </SafeAreaView>
+    );
+  }
+  return (
+    <SafeAreaView style={{ flex: 1 }} edges={["bottom"]}>
+      <Stack.Screen
+        options={{
+          headerTitle: locationData?.name || "Chi tiết vị trí",
+          headerShown: true,
+          headerRight: () => (
+            <Badge status={locationData?.status || "CLOSED"} />
+          ),
+        }}
+      />
+      <LocationDetailScreen data={locationData} />
+      <Dialog visible={dialogVisible} onDismiss={() => setDialogVisible(false)}>
+        <Dialog.Title>Lỗi</Dialog.Title>
+        <Dialog.Content>
+          <Text>
+            {locationData?.message || "Đã xảy ra lỗi khi lấy dữ liệu vị trí."}
+          </Text>
+        </Dialog.Content>
+        <Dialog.Actions>
+          <Button onPress={() => setDialogVisible(false)}>Đóng</Button>
+        </Dialog.Actions>
+      </Dialog>
+    </SafeAreaView>
+  );
+}
