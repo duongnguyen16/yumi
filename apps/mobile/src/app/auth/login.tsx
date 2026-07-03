5118807374c910f5a12bbd26d062d32c56187079
Long <baolong2000k4@gmail.com>
2026-07-03 18:25:25 +0700

    WDP-11 [S2] F07 - FIX location

--- apps/mobile/src/app/auth/login.tsx
+++ apps/mobile/src/app/auth/login.tsx
@@ -2,12 +2,22 @@ import LoginForm from "@/components/auth/LoginForm";
 import { userContext } from "@/contexts/userContext";
 import { Redirect, Stack } from "expo-router";
 import { useContext } from "react";
-import { KeyboardAvoidingView, Platform } from "react-native";
+import { KeyboardAvoidingView, Platform, View } from "react-native";
+import { ActivityIndicator } from "react-native-paper";
 
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Login() {
-  const { user } = useContext(userContext);
+  const { user, loading } = useContext(userContext);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   if (user) {
     return <Redirect href="/home" />;
   }
