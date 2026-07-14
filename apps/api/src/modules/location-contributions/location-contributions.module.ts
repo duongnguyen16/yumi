b50e90addbee418feef9ee21293952f0d755bd10
Tran Duc Minh <minhsoc5@gmail.com>
2026-07-14 09:16:13 +0700

    feat(api): upload contribution images on backend

--- apps/api/src/modules/location-contributions/location-contributions.module.ts
+++ apps/api/src/modules/location-contributions/location-contributions.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { AtStrategy } from 'src/common/guard/at.strategy';
 import { SchemaModule } from 'src/common/schemas/schema.module';
 import { DuplicateDetectionService } from '../duplicate-detection/duplicate-detection.service';
+import { ImagesModule } from '../images/images.module';
 import { LocationGeoService } from '../location-geo/location-geo.service';
 import { LocationContributionsController } from './location-contributions.controller';
 import { LocationContributionsService } from './location-contributions.service';
 
 @Module({
   controllers: [LocationContributionsController],
-  imports: [SchemaModule],
+  imports: [SchemaModule, ImagesModule],
   providers: [
     AtStrategy,
     LocationContributionsService,
